57618df88c6415ebd73d073fc5ed494ed0e081cb
ALEXIS <alexis5999@gmail.com>
2024-05-06 22:14:04 -0500

    se sube cambios del crud completo

--- Semana05/src/main/java/certus/edu/pe/repositorios/PeliculaRepositorio.java
+++ Semana05/src/main/java/certus/edu/pe/repositorios/PeliculaRepositorio.java
@@ -12,4 +12,10 @@ public interface PeliculaRepositorio extends CrudRepository<Pelicula, Integer> {
 	
 	@Query(value = "SELECT a FROM Pelicula a WHERE a.nombre =?1")
 	public List<Pelicula> buscarPeliculasPorNombre(String nombre);
+	
+	
+	@Query(value = "SELECT a FROM Pelicula a WHERE a.nombre like CONCAT(?1,'%')")
+	public List<Pelicula> buscarPeliculaLikeNombre(String nombre);
+	
+	
 }
--- Semana05/src/main/java/certus/edu/pe/servicios/PeliculaServicio.java
+++ Semana05/src/main/java/certus/edu/pe/servicios/PeliculaServicio.java
@@ -28,7 +28,33 @@ public class PeliculaServicio {
 		
 	}
 	
+	public Pelicula crear(Pelicula pelicula) {
+		return repositorio.save(pelicula);
+	}
+	
+	public Pelicula actualizar(Pelicula peliculaActualizar) {
+		Pelicula peliculaActual= repositorio.findById(peliculaActualizar.getIdPelicula()).get();
+		//peliculaActual.setIdPelicula(peliculaActualizar.getIdPelicula());
+		peliculaActual.setNombre(peliculaActualizar.getNombre());
+		peliculaActual.setDuracion(peliculaActual.getDuracion());
+		peliculaActual.setClasificacion(peliculaActualizar.getClasificacion());
+		peliculaActual.setIdioma(peliculaActualizar.getIdioma());
+		peliculaActual.setGenero(peliculaActualizar.getGenero());
+		peliculaActual.setFormato(peliculaActualizar.getFormato());
+		peliculaActual.setSinopsis(peliculaActualizar.getSinopsis());
+		
+		Pelicula peliculaActualizado = repositorio.save(peliculaActual);
+		return peliculaActualizado;
+	
+	}
+	
+	public Pelicula buscarPorId(Integer id) {
+		return repositorio.findById(id).get();
+	}
 
+	public void borrarPorId(Integer id) {
+		repositorio.deleteById(id);
+	}
 	
 	
 }
